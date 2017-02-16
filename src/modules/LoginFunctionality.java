b3815987ffc9a47d357a06f66224941cdd015036
Jeeva <Jeeva@192.168.0.3>
2017-02-16 11:11:28 +0530

    LF updated

---
+++ src/modules/LoginFunctionality.java
@@ -0,0 +1,23 @@
+package modules;
+
+import org.openqa.selenium.WebDriver;
+import pageObjects.LoginPage;
+
+public class LoginFunctionality
+{
+	WebDriver driver;
+	//Constructor
+	public LoginFunctionality(WebDriver driver)
+	{
+		this.driver = driver;
+	}
+	
+	public void LoginExec(String username,String password)
+	{
+		LoginPage lp = new LoginPage(driver);
+		lp.lnkLogin().click();
+		lp.txtboxUsername().sendKeys(username);
+		lp.txtboxPassword().sendKeys(password);
+		lp.btnLogin().click();
+	}
+}
